a6926264d3b5131f28a584fed66eccca40245102
Keith R Languet <kralnguet.gmail.com>
2019-05-11 02:36:18 -0500

    Wrote first reddit script to read saved items!

---
+++ RosiesRedditArchiver.py
@@ -0,0 +1,16 @@
+import praw
+from praw.models.reddit.submission import Submission 
+from praw.models.reddit.comment import Comment 
+from pprint import pprint as prnt
+
+reddit = praw.Reddit('RosiesRedditArchiver')
+print("Full user account access: ", not reddit.read_only)
+
+print("Saved Items:")
+for item in reddit.redditor('krlanguet').saved(limit=9):
+    if isinstance(item, Submission):
+        print("Post title:", item.title)
+    elif isinstance(item, Comment):
+        print('Comment by:', item.author.name)
+    else:
+        print('Unrecognized type:', item.type)
