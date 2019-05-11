b7e71c392599db3935ac6c74392ba3a1e91d3ce9
Keith R Languet <krlanguet@gmail.com>
2019-05-11 13:10:09 -0500

    Parametrized limit with script input, and made output accessible from interactive mode.

--- RosiesRedditArchiver.py
+++ RosiesRedditArchiver.py
@@ -1,17 +1,28 @@
 import praw
 from praw.models.reddit.submission import Submission 
-from praw.models.reddit.comment import Comment 
-from pprint import pprint as prnt
+from praw.models.reddit.comment import Comment
 
+from pprint import pprint
+
+from sys import argv
+
+def explore(obj):
+    pprint(vars(obj))
+
+print("Connecting to reddit")
 reddit = praw.Reddit('RosiesRedditArchiver')
 print("Full user account access: ", not reddit.read_only)
 
+saved_items=[]
 print("Saved Items:")
-for item in reddit.redditor('krlanguet').saved(limit=20):
+for item in reddit.redditor('krlanguet').saved(
+	limit=None if len(argv) <=1 else int(argv[1])
+):
     print(" * ", end='')
     if isinstance(item, Submission):
-        print("Post title:", item.title)
+        print("Post \"%s\" in %s" % (item.title, item.subreddit._path))
     elif isinstance(item, Comment):
-        print('Comment by:', item.author.name)
+        print('Comment by /u/%s in %s' % (item.author.name, item.subreddit._path))
     else:
         print('Unrecognized type:', item.type)
+    saved_items.append(item)
